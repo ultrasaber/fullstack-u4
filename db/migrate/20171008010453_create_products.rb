b44732f850e7d204d2f388d68d882e1cf66db7f9
ultrasaber <saberthesaber@gmail.com>
2017-10-08 01:10:43 +0000

    Fixed typo in migration script.

--- db/migrate/20171008010453_create_products.rb
+++ db/migrate/20171008010453_create_products.rb
@@ -2,7 +2,7 @@ class CreateProducts < ActiveRecord::Migration[5.1]
   def change
     create_table :products do |t|
       t.string :name
-      t.test :description
+      t.text :description
       t.decimal :price
       t.integer :stock_quantity
 
